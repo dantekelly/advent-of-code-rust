c2e828763d7c0e1ada80d0c174ac671c8217a09e
Dante Kelly <dante@dantekelly.com>
2024-12-08 15:23:39 -0800

    Add part 2 progress

--- 2024/day_03/src/bin/part_2.rs
+++ 2024/day_03/src/bin/part_2.rs
@@ -1,16 +1,38 @@
-use regex::Regex;
 use std::fs;
+use std::ops::Range;
 
 fn solve_challenge(input: &str) -> u32 {
-    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-
     input
         .lines()
         .map(|line| {
             let mul_indeces = line.match_indices("mul(").collect::<Vec<_>>();
+            dbg!(&mul_indeces);
+
+            /* Unavailable Ranges:
+             * How to get the unavailable ranges:
+             * 1. Find all the don't() and do() indices
+             * 2. For each don't(), find the next do() that is larger than it
+             * 3. Add the range to the unavailable ranges
+             */
+            let unavailable_ranges: Vec<Range<usize>> = line
+                // Find all the don't() indices
+                .match_indices("don't()")
+                // Find the next do() that is larger than it or use line length
+                .map(|dont_index| {
+                    match line
+                        .match_indices("do()")
+                        .find(|do_index| do_index.0 > dont_index.0)
+                    {
+                        Some(do_index) => dont_index.0..do_index.0,
+                        None => dont_index.0..line.len(),
+                    }
+                })
+                .collect();
+            dbg!(&unavailable_ranges);
+
             let mut matches: Vec<u32> = vec![];
 
-            let mut max_matches = mul_indeces.len();
+            let max_matches = mul_indeces.len();
             let mut current_match = 0;
             let mut index = mul_indeces[current_match].0 + 4;
 
@@ -18,9 +40,25 @@ fn solve_challenge(input: &str) -> u32 {
             let mut first_number = String::new();
             let mut first_number_finished = false;
             let mut second_number = String::new();
-            let mut second_number_finished = false;
 
             while index < line.chars().count() {
+                if unavailable_ranges
+                    .iter()
+                    .any(|range| range.contains(&index))
+                {
+                    // dbg!("Bad Range");
+                    if current_match + 1 < max_matches {
+                        current_match += 1;
+                        index = mul_indeces[current_match].0 + 4;
+                        continue;
+                    } else {
+                        dbg!("No more matches");
+                        break;
+                    }
+                }
+
+                dbg!(&line.chars().nth(index).unwrap());
+                dbg!(&index);
                 if !first_number_finished {
                     if line.chars().nth(index).unwrap().is_digit(10) {
                         first_number = format!(
@@ -32,21 +70,21 @@ fn solve_challenge(input: &str) -> u32 {
                         first_number_finished = true;
                         dbg!("Found comma, first number is finished");
                     } else {
-                        // GOTO Next Match
                         dbg!(
                             "Invalid character for first number: {}",
-                            line.chars().nth(index).unwrap()
+                            // line.chars().nth(index).unwrap()
                         );
+                        dbg!(line.chars().nth(index).unwrap());
                         first_number = String::new();
                         first_number_finished = false;
-                        if current_match + 1 < max_matches {
+                        if current_match + 1 <= max_matches {
                             current_match += 1;
                             index = mul_indeces[current_match].0 + 3;
                         } else {
                             break;
                         }
                     }
-                } else if !second_number_finished {
+                } else {
                     if line.chars().nth(index).unwrap().is_digit(10) {
                         second_number = format!(
                             "{}{}",
@@ -64,7 +102,6 @@ fn solve_challenge(input: &str) -> u32 {
                         first_number = String::new();
                         first_number_finished = false;
                         second_number = String::new();
-                        second_number_finished = false;
 
                         if current_match + 1 < max_matches {
                             current_match += 1;
@@ -73,15 +110,10 @@ fn solve_challenge(input: &str) -> u32 {
                             break;
                         }
                     } else {
-                        // GOTO Next Match
-                        dbg!(
-                            "Invalid character for second number: {}",
-                            line.chars().nth(index).unwrap()
-                        );
                         first_number = String::new();
                         first_number_finished = false;
                         second_number = String::new();
-                        second_number_finished = false;
+
                         if current_match + 1 < max_matches {
                             current_match += 1;
                             index = mul_indeces[current_match].0 + 3;
@@ -89,8 +121,6 @@ fn solve_challenge(input: &str) -> u32 {
                             break;
                         }
                     }
-                } else {
-                    dbg!("Invalid State");
                 }
                 index += 1;
             }
@@ -104,6 +134,11 @@ fn solve_challenge(input: &str) -> u32 {
 fn main() {
     let input = fs::read_to_string("./src/bin/input.txt").unwrap();
 
+    // TOO LOW : 49416743
+    // TOO HIGH: 99812796
+    // TOO HIGH: 102489528
+    // TOO HIGH: 104804964
+
     println!("Answer: {}", solve_challenge(&input));
 }
 
@@ -113,8 +148,8 @@ mod tests {
 
     #[test]
     fn test_example() {
-        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
-        assert_eq!(solve_challenge(input), 161)
+        assert_eq!(solve_challenge(input), 48)
     }
 }
