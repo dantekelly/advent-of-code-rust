09b7c0b95d20f4bdaa2beb914826f2a3e72eb4fe
Dante Kelly <dante@dantekelly.com>
2024-12-08 16:52:07 -0800

    Add final solution to day 3 part 2

--- 2024/day_03/src/bin/part_1.rs
+++ 2024/day_03/src/bin/part_1.rs
@@ -1,18 +1,15 @@
 use regex::Regex;
 use std::fs;
 
-fn solve_challenge(input: String) -> i32 {
+fn solve_challenge(input: &str) -> u32 {
     let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-
     input
         .lines()
-        .map(|line| {
-            re.captures_iter(line)
-                .map(|c| {
-                    let [n1, n2] = c.extract::<2>().1; // Destructure the array
-                    n1.parse::<i32>().unwrap() * n2.parse::<i32>().unwrap()
-                })
-                .sum::<i32>()
+        .flat_map(|line| {
+            re.captures_iter(line).map(|c| {
+                let [n1, n2] = c.extract::<2>().1;
+                n1.parse::<u32>().unwrap() * n2.parse::<u32>().unwrap()
+            })
         })
         .sum()
 }
@@ -20,7 +17,7 @@ fn solve_challenge(input: String) -> i32 {
 fn main() {
     let input = fs::read_to_string("./src/bin/input.txt").unwrap();
 
-    println!("Answer: {}", solve_challenge(input));
+    println!("Answer: {}", solve_challenge(&input));
 }
 
 #[cfg(test)]
@@ -29,9 +26,7 @@ mod tests {
 
     #[test]
     fn test_example() {
-        let input =
-            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string();
-
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!(solve_challenge(input), 161)
     }
 }
--- 2024/day_03/src/bin/part_2.rs
+++ 2024/day_03/src/bin/part_2.rs
@@ -1,143 +1,69 @@
-use std::fs;
-use std::ops::Range;
+use std::{fs, ops::Range};
+
+fn parse_mul(line: &str, start: usize) -> Option<u32> {
+    let mut chars = line[start..].chars();
+    let mut first_number = String::new();
+    let mut second_number = String::new();
+
+    // Skip mul(
+    for _ in 0..4 {
+        chars.next();
+    }
+
+    for c in chars.by_ref() {
+        if c.is_ascii_digit() {
+            first_number.push(c);
+        } else if c == ',' {
+            break;
+        } else {
+            return None;
+        }
+    }
+
+    for c in chars.by_ref() {
+        if c.is_ascii_digit() {
+            second_number.push(c);
+        } else if c == ')' {
+            break;
+        } else {
+            return None;
+        }
+    }
+
+    Some(first_number.parse::<u32>().ok()? * second_number.parse::<u32>().ok()?)
+}
 
 fn solve_challenge(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let mul_indeces = line.match_indices("mul(").collect::<Vec<_>>();
-            dbg!(&mul_indeces);
-
-            /* Unavailable Ranges:
-             * How to get the unavailable ranges:
-             * 1. Find all the don't() and do() indices
-             * 2. For each don't(), find the next do() that is larger than it
-             * 3. Add the range to the unavailable ranges
-             */
-            let unavailable_ranges: Vec<Range<usize>> = line
-                // Find all the don't() indices
-                .match_indices("don't()")
-                // Find the next do() that is larger than it or use line length
-                .map(|dont_index| {
-                    match line
-                        .match_indices("do()")
-                        .find(|do_index| do_index.0 > dont_index.0)
-                    {
-                        Some(do_index) => dont_index.0..do_index.0,
-                        None => dont_index.0..line.len(),
-                    }
-                })
-                .collect();
-            dbg!(&unavailable_ranges);
-
-            let mut matches: Vec<u32> = vec![];
-
-            let max_matches = mul_indeces.len();
-            let mut current_match = 0;
-            let mut index = mul_indeces[current_match].0 + 4;
-
-            // Numbers can be 1, 2 or 3 digits
-            let mut first_number = String::new();
-            let mut first_number_finished = false;
-            let mut second_number = String::new();
-
-            while index < line.chars().count() {
-                if unavailable_ranges
-                    .iter()
-                    .any(|range| range.contains(&index))
-                {
-                    // dbg!("Bad Range");
-                    if current_match + 1 < max_matches {
-                        current_match += 1;
-                        index = mul_indeces[current_match].0 + 4;
-                        continue;
-                    } else {
-                        dbg!("No more matches");
-                        break;
-                    }
-                }
-
-                dbg!(&line.chars().nth(index).unwrap());
-                dbg!(&index);
-                if !first_number_finished {
-                    if line.chars().nth(index).unwrap().is_digit(10) {
-                        first_number = format!(
-                            "{}{}",
-                            first_number,
-                            line.chars().nth(index).unwrap().to_string()
-                        );
-                    } else if line.chars().nth(index).unwrap() == ',' {
-                        first_number_finished = true;
-                        dbg!("Found comma, first number is finished");
-                    } else {
-                        dbg!(
-                            "Invalid character for first number: {}",
-                            // line.chars().nth(index).unwrap()
-                        );
-                        dbg!(line.chars().nth(index).unwrap());
-                        first_number = String::new();
-                        first_number_finished = false;
-                        if current_match + 1 <= max_matches {
-                            current_match += 1;
-                            index = mul_indeces[current_match].0 + 3;
-                        } else {
-                            break;
-                        }
-                    }
-                } else {
-                    if line.chars().nth(index).unwrap().is_digit(10) {
-                        second_number = format!(
-                            "{}{}",
-                            second_number,
-                            line.chars().nth(index).unwrap().to_string()
-                        );
-                    } else if line.chars().nth(index).unwrap() == ')' {
-                        dbg!("Found closing parenthesis, second number is finished");
-                        dbg!(&first_number);
-                        dbg!(&second_number);
-                        matches.push(
-                            first_number.parse::<u32>().unwrap_or_default()
-                                * second_number.parse::<u32>().unwrap_or_default(),
-                        );
-                        first_number = String::new();
-                        first_number_finished = false;
-                        second_number = String::new();
-
-                        if current_match + 1 < max_matches {
-                            current_match += 1;
-                            index = mul_indeces[current_match].0 + 3;
-                        } else {
-                            break;
-                        }
-                    } else {
-                        first_number = String::new();
-                        first_number_finished = false;
-                        second_number = String::new();
-
-                        if current_match + 1 < max_matches {
-                            current_match += 1;
-                            index = mul_indeces[current_match].0 + 3;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-                index += 1;
+    let unavailable_ranges: Vec<Range<usize>> = input
+        .match_indices("don't()")
+        .map(|(start, _)| {
+            match input
+                .match_indices("do()")
+                .find(|&(do_start, _)| do_start > start)
+            {
+                Some((end, _)) => start..end,
+                None => start..input.len(),
             }
+        })
+        .collect();
 
-            dbg!(&matches);
-            matches.into_iter().sum::<u32>()
+    input
+        .match_indices("mul(")
+        .filter_map(|(start, _)| {
+            if unavailable_ranges
+                .iter()
+                .any(|range| range.contains(&start))
+            {
+                None
+            } else {
+                parse_mul(input, start)
+            }
         })
         .sum()
 }
 
 fn main() {
     let input = fs::read_to_string("./src/bin/input.txt").unwrap();
-
-    // TOO LOW : 49416743
-    // TOO HIGH: 99812796
-    // TOO HIGH: 102489528
-    // TOO HIGH: 104804964
 
     println!("Answer: {}", solve_challenge(&input));
 }
